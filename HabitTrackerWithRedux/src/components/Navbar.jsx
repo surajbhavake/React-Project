ce5dcb4584ce07e5bf936166fe03b512b00b2bbc
Suraj Bhavake <surajbhavake2@gmail.com>
2026-06-26 03:02:47 +0530

    chore : done making nav bar

--- HabitTrackerWithRedux/src/components/Navbar.jsx
+++ HabitTrackerWithRedux/src/components/Navbar.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Navbar() {
+
+
+
   return (
     <div>
-      <h1>Navbar</h1>
+     <nav className='bg-white shadow-sm border-b border-gray-200 flex justify-between items-center px-8 py-4'>
+      <Link to='/' className='text-xl font-bold text-blue-600'>Habit Tracker</Link>
+      <div className='flex gap-4'>
+      <Link to='/' className='hover:text-blue-600'>Dashboard</Link>
+      <Link to='/about' className='hover:text-blue-600'>About</Link>
+      <Link to= '/settings' className='hover:text-blue-600'>Setting</Link>
+      <Link to='/statistics' className='hover:text-blue-600'>Statistics</Link>
+      </div>
+     </nav>
     </div>
   )
 }
