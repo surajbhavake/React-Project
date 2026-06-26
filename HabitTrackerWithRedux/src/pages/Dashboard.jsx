210dbcf257b5298d07b19ae1b829de3a284013d6
Suraj Bhavake <surajbhavake2@gmail.com>
2026-06-26 19:19:25 +0530

    chore : added Search bar

--- HabitTrackerWithRedux/src/pages/Dashboard.jsx
+++ HabitTrackerWithRedux/src/pages/Dashboard.jsx
@@ -2,8 +2,10 @@ import React, { useState } from 'react'
 import HabitForm from '../components/HabitForm'
 import HabitCard from '../components/HabitCard'
 import ProgressCard from '../components/ProgressCard'
+import FilterButtons from '../components/FilterButtons'
 import { addHabit,toggleHabit,deleteHabit,CompletedCount,updateHabit } from '../store/HabitsSlice'
 import { useDispatch, useSelector } from 'react-redux'
+import SearchBar from '../components/SearchBar'
 
 
 function Dashboard() {
@@ -11,6 +13,7 @@ function Dashboard() {
   const[habitName,setHabitName]= useState('')
   const[editingId,setEditingId] = useState(null)
   const[filter,setFilter] = useState('all')
+  const[searchTerm,setSearchTerm] = useState('')
 
 
 
@@ -29,6 +32,12 @@ function Dashboard() {
   if(filter === 'pending'){
     displayHabits = habits.filter((h)=>(
       !h.CompletedCount
+    ))
+  }
+
+  if(searchTerm.trim()){
+    displayHabits = displayHabits.filter((h)=>(
+      h.habitName.toLowerCase().includes(searchTerm.toLowerCase())
     ))
   }
 
@@ -62,6 +71,9 @@ function Dashboard() {
     <div className='p-6 max-w-4xl mx-auto space-y-6'>
       <h1>Dashboard</h1>
 
+      <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm}/>
+      <FilterButtonss/>
+
       <ProgressCard
       completed = {completedCount}
       total = {habits.length}
@@ -85,6 +97,10 @@ function Dashboard() {
 
           />
         ))}
+
+        {displayHabits.length === 0 && (
+          <p className="text-gray-500 text-center">No habits found</p>
+        )}
       </div>
     </div>
 
